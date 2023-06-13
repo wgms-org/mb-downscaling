364e435a83647bda496d7515278a1022e571074d
ezwelty <ethan.welty@gmail.com>
2023-06-13 09:59:42 +0200

    Restore original argument order and names
    Ensures backwards compatibility for evaluating influence of changes on
    results.

--- mass_balance_interpolations.py
+++ mass_balance_interpolations.py
@@ -9,10 +9,10 @@ import numpy as np
 
 
 def sine_interpolation_from_mean_balances(
-    annual_balance: float,
     balance_amplitude: float,
-    winter_fraction: float = 0.5,
+    annual_balance: float,
     temporal_resolution: int = 365,
+    winter_fraction: float = 0.5,
     uniform_annual_balance: bool = True
 ) -> pd.DataFrame:
     """
@@ -27,12 +27,12 @@ def sine_interpolation_from_mean_balances(
     `winter_fraction`, `annual_balance`, and `balance_amplitude`.
 
     Arguments:
-        annual_balance: Annual mass balance.
         balance_amplitude: Mass-balance amplitude
             (see `calc_mass_balance_amplitude`).
-        winter_fraction: Annual fraction of winter season.
+        annual_balance: Annual mass balance.
         temporal_resolution: Temporal resolution of output,
             e.g. 12 (~monthly) or 365 (~daily).
+        winter_fraction: Annual fraction of winter season.
         uniform_annual_balance: Whether annual balance should be applied
             uniformally over the year (True) rather than assume that all mass
             gain/loss occurs in winter/summer (False).
@@ -41,41 +41,41 @@ def sine_interpolation_from_mean_balances(
         Dataframe with columns TIME_STEP (float) and BALANCE (float).
 
     Examples:
-        >>> annual_balance = -3
         >>> balance_amplitude = 5
+        >>> annual_balance = -3
 
         With winter and summer of equal length:
 
         >>> balances = sine_interpolation_from_mean_balances(
-        ...     annual_balance, balance_amplitude
+        ...     balance_amplitude, annual_balance
         ... )['BALANCE']
         >>> np.isclose(annual_balance, balances.sum(), atol=1e-5)
         True
         >>> np.isclose(
         ...     balance_amplitude,
         ...     abs(balances[:182].sum() - balances[182:].sum()) / 2,
-        ...     atol=1e-4
+        ...     atol=1e-2
         ... )
         True
 
         With winter and summer of different lengths:
 
         >>> balances = sine_interpolation_from_mean_balances(
-        ...     annual_balance, balance_amplitude, winter_fraction=0.75
+        ...     balance_amplitude, annual_balance, winter_fraction=0.75
         ... )['BALANCE']
         >>> np.isclose(annual_balance, balances.sum(), atol=1e-3)
         True
         >>> np.isclose(
         ...     balance_amplitude,
         ...     abs(balances[:273].sum() - balances[273:].sum()) / 2,
-        ...     atol=1e-3
+        ...     atol=1e-2
         ... )
         True
 
         And with non-uniform annual balance:
 
         >>> balances = sine_interpolation_from_mean_balances(
-        ...     annual_balance, balance_amplitude, winter_fraction=0.75,
+        ...     balance_amplitude, annual_balance, winter_fraction=0.75,
         ...     uniform_annual_balance=True
         ... )['BALANCE']
         >>> np.isclose(annual_balance, balances.sum(), atol=1e-3)
@@ -83,7 +83,7 @@ def sine_interpolation_from_mean_balances(
         >>> np.isclose(
         ...     balance_amplitude,
         ...     abs(balances[:273].sum() - balances[273:].sum()) / 2,
-        ...     atol=1e-3
+        ...     atol=1e-2
         ... )
         True
     """
@@ -121,8 +121,8 @@ def sine_interpolation_from_mean_balances(
 def sine_interpolation_from_seasonal_balances(
     winter_balance: float,
     summer_balance: float,
-    winter_fraction: float = 0.5,
-    temporal_resolution: int = 365
+    temporal_resolution: int = 365,
+    winter_fraction: float = 0.5
 ) -> pd.DataFrame:
     """
     Interpolate mass balance for a full year from seasonal balances.
@@ -138,9 +138,9 @@ def sine_interpolation_from_seasonal_balances(
     Arguments:
         winter_balance: Winter mass balance.
         summer_balance: Summer mass balance.
-        winter_fraction: Annual fraction of winter season.
         temporal_resolution: Temporal resolution of output,
             e.g. 12 (~monthly) or 365 (~daily).
+        winter_fraction: Annual fraction of winter season.
 
     Returns:
         Dataframe with columns TIME_STEP (float) and BALANCE (float).
@@ -165,7 +165,7 @@ def sine_interpolation_from_seasonal_balances(
 
 def interpolate_daily_balances(
     bwsa_df: pd.DataFrame,
-    balance_amplitude: float = None,
+    alpha: float = None,
     winter_fraction: float = 0.5,
     winter_start: Tuple[bool, int, int] = (False, 10, 1),
     uniform_annual_balance: bool = True
@@ -180,7 +180,7 @@ def interpolate_daily_balances(
         bwsa_df: DataFrame with columns
             Year (int), WINTER_BALANCE (float), SUMMER_BALANCE (float),
             and ANNUAL_BALANCE (float).
-        balance_amplitude: Mass-balance amplitude. If not provided,
+        alpha: Mass-balance amplitude. If not provided,
             it is computed from the seasonal balances in `bwsa_df`
             (see `calc_mass_balance_amplitude`).
         winter_fraction: Annual fraction of winter season.
@@ -194,8 +194,8 @@ def interpolate_daily_balances(
         Dataframe with index DATE (datetime) and column BALANCE (float).
     """
     is_start_year, month, day = winter_start
-    if balance_amplitude is None:
-        balance_amplitude = calc_mass_balance_amplitude(bwsa_df)
+    if alpha is None:
+        alpha = calc_mass_balance_amplitude(bwsa_df)
     years = []
     for row in bwsa_df.to_dict(orient='records'):
         # Create series of every date in the hydrological year
@@ -211,7 +211,7 @@ def interpolate_daily_balances(
             # Use annual balance
             balances = sine_interpolation_from_mean_balances(
                 annual_balance=row['ANNUAL_BALANCE'],
-                balance_amplitude=balance_amplitude,
+                balance_amplitude=alpha,
                 winter_fraction=winter_fraction,
                 temporal_resolution=n_dates,
                 uniform_annual_balance=uniform_annual_balance
